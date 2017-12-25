12151b9ce7bd42393e868b515baf8aec8e8a3ed8
Alex Prokudin <prokudin89@gmail.com>
2017-12-25 21:47:51 +0700

    spring init

---
+++ src/main/java/com/borodagames/trainingnotebook/TrainingnotebookApplication.java
@@ -0,0 +1,12 @@
+package com.borodagames.trainingnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingnotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingnotebookApplication.class, args);
+	}
+}
